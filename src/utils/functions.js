212818e4f0cbd41a8d9086f23be36abcef70c488
haith2m <haitham.almaymouni@outlook.com>
2023-12-14 23:07:40 +0300

    Refactor functions in functions.js

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,166 +1,227 @@
-    const {boards} = require('../models/BoardModel');
-    module.exports = (interaction) => {
-async function createBoard(name, user) {
-    const boardExist = await boards.findOne({ name, user: user });
-    if (boardExist) {
-        throw new Error(`Board **${name}** already exists!`);
-    return 
-     }
+const { boards } = require('../models/BoardModel');
 
-    const board = new boards({
+module.exports = () => {
+  async function createBoard(name, user) {
+    try {
+      const boardExist = await boards.findOne({ name, user: user });
+      if (boardExist) {
+        throw new Error(`Board **${name}** already exists!`);
+      }
+
+      const board = new boards({
         name,
         tasks: [],
         members: [],
         user: user,
-    });
+      });
 
-    await board.save();
-}
+      await board.save();
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to create board.');
+    }
+  }
 
-async function deleteBoard(name, user) {
-    const board = await boards.findOne({ name, user: user }) || await boards.findOne({ name, members: { $elemMatch: { id: user } } }); 
-    if (!board) {
+  async function deleteBoard(name, user) {
+    try {
+      const board = await boards.findOne({ name, user: user }) || await boards.findOne({ name, members: { $elemMatch: { id: user } } });
+      if (!board) {
         throw new Error(`Board **${name}** not found!`);
-    } else if (board.user !== user) {
+      } else if (board.user !== user) {
         throw new Error(`You are not the owner of board **${name}**!`);
+      }
+      await boards.deleteOne({ name, user: user });
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to delete board.');
     }
-    await boards.deleteOne({ name, user: user });
-}
+  }
 
-async function getBoard(name, user) {
-    return await boards.findOne({ name, user: user }) || await boards.findOne({ name, members: { $elemMatch: { id: user } } });
-}
-
-async function addTaskToBoard(boardName, task, user) {
-    const board = await getBoard(boardName, user);
-    if (!board) {
-        throw new Error(`Board **${boardName}** not found!`);
+  async function getBoard(name, user) {
+    try {
+      return await boards.findOne({ name, user: user }) || await boards.findOne({ name, members: { $elemMatch: { id: user } } });
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to get board.');
     }
+  }
 
-    board.tasks.push(task);
-    board.markModified('tasks');
-    await board.save();
-}
-
-async function removeTaskFromBoard(boardName, task, user) {
-    const board = await getBoard(boardName, user);
-    if (!board) {
+  async function addTaskToBoard(boardName, task, user) {
+    try {
+      const board = await getBoard(boardName, user);
+      if (!board) {
         throw new Error(`Board **${boardName}** not found!`);
-    } else if (!board.tasks.find(t => t.name === task)) {
+      }
+
+      board.tasks.push(task);
+      board.markModified('tasks');
+      await board.save();
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to add task to board.');
+    }
+  }
+
+  async function removeTaskFromBoard(boardName, task, user) {
+    try {
+      const board = await getBoard(boardName, user);
+      if (!board) {
+        throw new Error(`Board **${boardName}** not found!`);
+      } else if (!board.tasks.find(t => t.name === task)) {
         throw new Error(`Task **${task}** not found in board **${boardName}**!`);
-    } else if (board.user !== user) {
+      } else if (board.user !== user) {
         throw new Error(`You are not the owner of board **${boardName}**!`);
+      }
+
+      board.tasks = board.tasks.filter(t => t.name !== task);
+      board.markModified('tasks');
+      await board.save();
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to remove task from board.');
     }
+  }
 
-    board.tasks = board.tasks.filter(t => t.name !== task);
-    board.markModified('tasks');
-    await board.save();
-}
-
-async function listTasks(boardName, user) {
-    const board = await getBoard(boardName, user);
-    if (!board) {
+  async function listTasks(boardName, user) {
+    try {
+      const board = await getBoard(boardName, user);
+      if (!board) {
         throw new Error(`Board **${boardName}** not found!`);
+      }
+
+      return board.tasks;
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to list tasks.');
     }
+  }
 
-    return board.tasks;
-}
+  async function moveTaskBetweenBoards(fromBoardName, toBoardName, task, user) {
+    try {
+      const fromBoard = await getBoard(fromBoardName, user);
+      const toBoard = await getBoard(toBoardName, user);
 
-async function moveTaskBetweenBoards(fromBoardName, toBoardName, task, user) {
-    const fromBoard = await getBoard(fromBoardName, user);
-    const toBoard = await getBoard(toBoardName, user);
-
-    if (!fromBoard || !toBoard) {
+      if (!fromBoard || !toBoard) {
         throw new Error(`Either board **${fromBoardName}** or board **${toBoardName}** not found!`);
-    }
+      }
 
-    const taskToMove = fromBoard.tasks.find(t => t.name === task);
-    if (!taskToMove) {
+      const taskToMove = fromBoard.tasks.find(t => t.name === task);
+      if (!taskToMove) {
         throw new Error(`Task **${task}** not found in board **${fromBoardName}**!`);
+      }
+
+      fromBoard.tasks = fromBoard.tasks.filter(t => t.name !== task);
+      toBoard.tasks.push(taskToMove);
+      fromBoard.markModified('tasks');
+      await fromBoard.save();
+      toBoard.markModified('tasks');
+      await toBoard.save();
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to move task between boards.');
     }
+  }
 
-    fromBoard.tasks = fromBoard.tasks.filter(t => t.name !== task);
-    toBoard.tasks.push(taskToMove);
-    fromBoard.markModified('tasks');
-    await fromBoard.save();
-    toBoard.markModified('tasks');
-    await toBoard.save();
-}
-
-async function changeTaskStatus(boardName, task, status, user) {
-    const board = await getBoard(boardName, user);
-    if (!board) {
+  async function changeTaskStatus(boardName, task, status, user) {
+    try {
+      const board = await getBoard(boardName, user);
+      if (!board) {
         throw new Error(`Board **${boardName}** not found!`);
-    }
+      }
 
-    const taskToChange = board.tasks.find(t => t.name === task);
-    if (!taskToChange) {
+      const taskToChange = board.tasks.find(t => t.name === task);
+      if (!taskToChange) {
         throw new Error(`Task **${task}** not found in board **${boardName}**!`);
+      }
+
+      taskToChange.status = status;
+      taskToChange.last_updated = Date.now();
+
+      board.markModified('tasks');
+      await board.save();
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to change task status.');
     }
+  }
 
-    taskToChange.status = status;
-    taskToChange.last_updated = Date.now();
-
-    board.markModified('tasks');
-    await board.save();
-}
-
-async function addMemberToBoard(boardName, member, user) {
-    const board = await getBoard(boardName, user);
-    if (!board) {
+  async function addMemberToBoard(boardName, member, user) {
+    try {
+      const board = await getBoard(boardName, user);
+      if (!board) {
         throw new Error(`Board **${boardName}** not found!`);
-    } else if (board.members.find(m => m.id === member.id)) {
+      } else if (board.members.find(m => m.id === member.id)) {
         throw new Error(`Member **${member}** already exists in board **${boardName}**!`);
+      }
+
+      board.members.push(member);
+      board.markModified('members');
+      await board.save();
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to add member to board.');
     }
+  }
 
-    board.members.push(member);
-    board.markModified('members');
-    await board.save();
-}
-
-async function removeMemberFromBoard(boardName, member, user) {
-    const board = await getBoard(boardName, user);
-    if (!board) {
+  async function removeMemberFromBoard(boardName, member, user) {
+    try {
+      const board = await getBoard(boardName, user);
+      if (!board) {
         throw new Error(`Board **${boardName}** not found!`);
-    } else if (!board.members.find(m => m.id === member.id)) {
+      } else if (!board.members.find(m => m.id === member.id)) {
         throw new Error(`Member **${member}** not found in board **${boardName}**!`);
+      }
+
+      board.members = board.members.filter(m => m.id !== member.id);
+      board.markModified('members');
+      await board.save();
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to remove member from board.');
     }
+  }
 
-    board.members = board.members.filter(m => m.id !== member.id);
-    board.markModified('members');
-    await board.save();
-}
-
-async function listMembers(boardName, user) {
-    const board = await getBoard(boardName, user);
-    if (!board) {
+  async function listMembers(boardName, user) {
+    try {
+      const board = await getBoard(boardName, user);
+      if (!board) {
         throw new Error(`Board **${boardName}** not found!`);
+      }
+
+      return board.members;
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to list members.');
     }
+  }
 
-    return board.members;
-}
-
-async function changeTaskPriority(boardName, task, priority, user) {
-    const board = await getBoard(boardName, user);
-    if (!board) {
+  async function changeTaskPriority(boardName, task, priority, user) {
+    try {
+      const board = await getBoard(boardName, user);
+      if (!board) {
         throw new Error(`Board **${boardName}** not found!`);
-    }
+      }
 
-    const taskToChange = board.tasks.find(t => t.name === task);
-    if (!taskToChange) {
+      const taskToChange = board.tasks.find(t => t.name === task);
+      if (!taskToChange) {
         throw new Error(`Task **${task}** not found in board **${boardName}**!`);
+      }
+
+      taskToChange.priority = priority.toLowerCase();
+      taskToChange.last_updated = Date.now();
+
+      board.markModified('tasks');
+      await board.save();
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to change task priority.');
     }
+  }
 
-    taskToChange.priority = priority.toLowerCase();
-    taskToChange.last_updated = Date.now();
-
-    board.markModified('tasks');
-    await board.save();
-}
-
-return {
+  return {
     createBoard,
     deleteBoard,
+    getBoard,
     addTaskToBoard,
     removeTaskFromBoard,
     listTasks,
@@ -170,7 +231,5 @@ return {
     removeMemberFromBoard,
     listMembers,
     changeTaskPriority,
-    getBoard
+  };
 };
-
-}
